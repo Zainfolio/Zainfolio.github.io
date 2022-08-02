772664107f67336a6a1a513f11bf27d5c9eef8b6
zainabd00 <110469755+zainabd00@users.noreply.github.com>
2022-08-02 17:29:12 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ===============================typing animation================================
+var typed = new Typed (".typing",{
+    strings:["","Web Developer" , "Web Designer" , "Graphic Designer", "Youtuber"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
